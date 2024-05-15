8427e1b3881ec24357ef7258fb8ba640f7413433
SaraSalazar <sara.salazar@genstudents.org>
2024-05-15 16:54:59 -0500

    Carpeta Hackaton

---
+++ app.js
@@ -0,0 +1,21 @@
+/* Configuración para el carrusel */
+const imgsCarrusel = document.querySelector('.contenedor-carrusel');
+const punto = document.querySelectorAll('.punto');
+
+// Se averigua la posición del punto al hacer click
+// Se recorren todas las class "punto"
+punto.forEach((cadaPunto,i)=>{
+  punto[i].addEventListener('click',()=>{
+    let posicion = i;
+
+    // Se aplica un transformX a "imgsCarrusel" (posicion 0 --> 0%, posicion 1 --> -50%)
+    let operacion = posicion * -33.33;
+    imgsCarrusel.style.transform = `translateX(${operacion}%)`;
+
+    // Se recorren los puntos para  actualizar la clase "activo"
+    punto.forEach((cadaPunto, i)=>{
+      punto[i].classList.remove('activo');
+    })
+    punto[i].classList.add('activo');
+  })
+})
